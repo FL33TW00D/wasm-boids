db5bec7f57056d5c5cc2509e29171bb80cf5b33f
Christopher Fleetwood <fleetwoodpersonal@gmail.com>
2021-08-06 20:51:13 +0100

    Working wasm version of boids

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+//TODO
+//Threading
+//Benchmarking
+//web
+#![feature(allocator_api)]
 mod utils;
 
 extern crate js_sys;
+extern crate web_sys;
+
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 use kdtree::distance::squared_euclidean;
-use kdtree::ErrorKind;
 use kdtree::KdTree;
 
 use wasm_bindgen::prelude::*;
@@ -12,7 +24,9 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-struct Starling {
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Starling {
     x: f32,
     y: f32,
     dx: f32,
@@ -30,14 +44,16 @@ impl Starling {
     }
 }
 
+#[wasm_bindgen]
 pub struct Murmuration {
     size: u32,
     width: u32,
     height: u32,
     flock: Vec<Starling>,
-    speed_limit: u32,
-    visual_field: u32,
-    seperation_distance: u32,
+    tree: KdTree<f32, usize, [f32; 2]>,
+    speed_limit: f32,
+    visual_field: f32,
+    seperation_distance: f32,
     seperation_coefficient: f32,
     alignment_coefficient: f32,
     cohesion_coefficient: f32,
@@ -45,69 +61,193 @@ pub struct Murmuration {
     boundary_coefficient: f32,
 }
 
-impl Murmuration {
-    pub fn tick(&mut self) {
-        for starling in self.flock.iter(){
-            let local = get_neighbours(starling, self.seperation_distance);
-            let visual = get_neighbours(starling, self.seperation_distance);
-
-            
-        }
+fn build_tree(flock: &Vec<Starling>) -> KdTree<f32, usize, [f32; 2]> {
+    let mut tree = KdTree::new(2);
+    for (idx, starling) in flock.iter().enumerate() {
+        tree.add([starling.x, starling.y], idx).unwrap();
     }
+    tree
+}
 
-    fn new() -> Murmuration {
-        let width = 1000;
-        let height = 1000;
-        let size = 500;
+#[wasm_bindgen]
+impl Murmuration {
+    pub fn new() -> Murmuration {
+        utils::set_panic_hook();
+        let size = 750;
+        let width = 2560;
+        let height = 1440;
+        let speed_limit = 120.;
+        let visual_field = 6000.;
+        let seperation_distance = 300.;
+        let seperation_coefficient = 0.05;
+        let alignment_coefficient = 0.04;
+        let cohesion_coefficient = 0.01;
+        let boundary_margin = 150;
+        let boundary_coefficient = 0.75;
+
         let mut flock: Vec<Starling> = Vec::new();
-        for n in 0..size {
+        for _ in 0..size {
             flock.push(Starling::new(width, height));
         }
 
+        let tree = build_tree(&flock);
+
+        Murmuration {
+            size,
+            width,
+            height,
+            flock,
+            tree,
+            speed_limit,
+            visual_field,
+            seperation_distance,
+            seperation_coefficient,
+            alignment_coefficient,
+            cohesion_coefficient,
+            boundary_margin,
+            boundary_coefficient,
+        }
     }
 
-    //Switch these so that they do not modify the starling dx values directly
-    fn seperate(&self, starling: &mut Starling, neighbours: Vec<Starling>) {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn size(&self) -> u32 {
+        self.size
+    }
+
+    pub fn flock(&self) -> *const Starling {
+        self.flock.as_ptr()
+    }
+
+    pub fn tick(&mut self) {
+        self.tree = build_tree(&self.flock);
+        let mut new_flock = Vec::new();
+        for starling in self.flock.iter() {
+            let local_idxs = self.get_neighbours(starling, self.seperation_distance);
+            let visual_idxs = self.get_neighbours(starling, self.visual_field);
+
+            let (avg_x, avg_y) = self.cohere(&starling, &self.flock, &visual_idxs);
+            let (x_delta, y_delta) = self.seperate(&starling, &self.flock, &local_idxs);
+            let (avg_dx, avg_dy) = self.align(&self.flock, &visual_idxs);
+
+            let mut new_dx = starling.dx + avg_x + x_delta + avg_dx;
+            let mut new_dy = starling.dy + avg_y + y_delta + avg_dy;
+
+            let speed = (new_dx * new_dx) + (new_dy * new_dy);
+            if speed > self.speed_limit {
+                new_dx = (new_dx / speed) * self.speed_limit;
+                new_dy = (new_dy / speed) * self.speed_limit;
+            }
+
+            let mut new_starling = Starling {
+                x: starling.x + new_dx,
+                y: starling.y + new_dy,
+                dx: new_dx,
+                dy: new_dy,
+            };
+            self.check_bounds(
+                &new_starling.x,
+                &new_starling.y,
+                &mut new_starling.dx,
+                &mut new_starling.dy,
+            );
+            new_flock.push(new_starling);
+        }
+        self.flock = new_flock;
+    }
+
+    fn check_bounds(&self, xpos: &f32, ypos: &f32, dx: &mut f32, dy: &mut f32) {
+        if *xpos > (self.width - self.boundary_margin) as f32 {
+            *dx -= self.boundary_coefficient;
+        }
+        if *xpos < self.boundary_margin as f32 {
+            *dx += self.boundary_coefficient;
+        }
+        if *ypos > (self.height - self.boundary_margin) as f32 {
+            *dy -= self.boundary_coefficient;
+        }
+        if *ypos < self.boundary_margin as f32 {
+            *dy += self.boundary_coefficient;
+        }
+    }
+
+    fn seperate(
+        &self,
+        starling: &Starling,
+        flock: &Vec<Starling>,
+        neighbours: &Vec<usize>,
+    ) -> (f32, f32) {
         let mut x_delta = 0.;
         let mut y_delta = 0.;
-        for neighbour in neighbours.iter() {
-            x_delta += starling.x - neighbour.x;
-            y_delta += starling.y - neighbour.y;
+        for idx in neighbours.iter() {
+            x_delta += starling.x - flock.get(*idx).unwrap().x;
+            y_delta += starling.y - flock.get(*idx).unwrap().y;
         }
-
-        starling.dx += x_delta * self.seperation_coefficient;
-        starling.dy += y_delta * self.seperation_coefficient;
+        (
+            x_delta * self.seperation_coefficient,
+            y_delta * self.seperation_coefficient,
+        )
     }
 
-    fn align(&self, starling: &mut Starling, neighbours: &Vec<Starling>) {
+    fn align(&self, flock: &Vec<Starling>, neighbours: &Vec<usize>) -> (f32, f32) {
         let mut avg_dx = 0.;
         let mut avg_dy = 0.;
-        for neighbour in neighbours.iter() {
-            avg_dx += neighbour.dx;
-            avg_dy += neighbour.dy;
+        for idx in neighbours.iter() {
+            avg_dx += flock.get(*idx).unwrap().dx;
+            avg_dy += flock.get(*idx).unwrap().dy;
         }
+
         if neighbours.len() > 0 {
-            starling.dx += (avg_dx / neighbours.len() as f32) * self.alignment_coefficient;
-            starling.dy += (avg_dy / neighbours.len() as f32) * self.alignment_coefficient;
+            avg_dx = (avg_dx / neighbours.len() as f32) * self.alignment_coefficient;
+            avg_dy = (avg_dy / neighbours.len() as f32) * self.alignment_coefficient;
         }
+
+        (avg_dx, avg_dy)
     }
 
-    fn cohere(&self, starling: &mut Starling, neighbours: &Vec<Starling>) {
+    fn cohere(
+        &self,
+        starling: &Starling,
+        flock: &Vec<Starling>,
+        neighbours: &Vec<usize>,
+    ) -> (f32, f32) {
         let mut avg_x = 0.;
         let mut avg_y = 0.;
 
-        for neighbour in neighbours.iter() {
-            avg_x += neighbour.x;
-            avg_y += neighbour.y;
+        for idx in neighbours.iter() {
+            avg_x += flock.get(*idx).unwrap().x;
+            avg_y += flock.get(*idx).unwrap().y;
         }
 
         if neighbours.len() > 0 {
-            starling.dx +=
-                (avg_x / neighbours.len() as f32 - starling.x) * self.cohesion_coefficient;
-            starling.dy +=
-                (avg_y / neighbours.len() as f32 - starling.y) * self.cohesion_coefficient;
+            avg_x = (avg_x / neighbours.len() as f32 - starling.x) * self.cohesion_coefficient;
+            avg_y = (avg_y / neighbours.len() as f32 - starling.y) * self.cohesion_coefficient;
         }
+        (avg_x, avg_y)
     }
 
-    fn get_neighbours(starling: &Starling, range: u32) -> Vec<Starling> {}
+    fn get_neighbours(&self, starling: &Starling, range: f32) -> Vec<usize> {
+        //within returns a Vector of (distance from provided point, &data on that point)
+        let mut neighbour_idx = Vec::new();
+        let neighbours = self
+            .tree
+            .within(&[starling.x, starling.y], range, &squared_euclidean)
+            .unwrap();
+
+        for data in neighbours.iter() {
+            let id = *data.1;
+            //dumb way to check if the point found within the range isn't itself
+            if data.0 != 0. {
+                neighbour_idx.push(id);
+            }
+        }
+
+        neighbour_idx
+    }
 }
